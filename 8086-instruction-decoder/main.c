9bcff361861ca60b3746cfdbe371b70eaaf0edb8
tkinaba <tiago.inaba@gmail.com>
2024-08-12 21:41:03 -0300

    immediate to register and writes and loads (some)

--- 8086-instruction-decoder/main.c
+++ 8086-instruction-decoder/main.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define INSTR_MASK 0b11111100
 #define MOV_INSTR 0b10001000
@@ -10,6 +12,19 @@
 
 char *register_word_table[8] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
 char *register_byte_table[8] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
+char *rm_mem_mode[8] = {"bx + si", "bx + di", "bp + si", "bp + di",
+						"si",	   "di",	  "bp",		 "bx"};
+
+char *surround_string(char *to_surround, char *format) {
+	char *buffer = (char *)malloc(strlen(to_surround) + strlen(format));
+	sprintf(buffer, format, to_surround);
+	return buffer;
+}
+
+int16_t two_byte_integer(char high, unsigned char low) {
+	int16_t res = high << 8;
+	return res | low;
+}
 
 int main(int argc, char **argv) {
 	FILE *fptr;
@@ -68,10 +83,77 @@ int main(int argc, char **argv) {
 			// Memory mode - 8 bit displacement follows
 			case 0b01: {
 				instruction[2] = fgetc(fptr);
+				uint8_t displacement = instruction[2];
+				if (d_field) {
+					dest = w_field ? register_word_table[reg_field]
+								   : register_byte_table[reg_field];
+					src = rm_mem_mode[rm_field];
+					char *temp = malloc(strlen(src) + 8);
+					if (displacement > 0)
+						sprintf(temp, "%s + %d", src, displacement);
+					else strcpy(temp, src);
+					src = surround_string(temp, "[%s]");
+					free(temp);
+				} else {
+					dest = rm_mem_mode[rm_field];
+					src = w_field ? register_word_table[reg_field]
+								  : register_byte_table[reg_field];
+					char *temp = malloc(strlen(dest) + 8);
+					if (displacement > 0)
+						sprintf(temp, "%s + %d", dest, displacement);
+					else strcpy(temp, dest);
+					dest = surround_string(temp, "[%s]");
+					free(temp);
+				}
 			} break;
+			// Memory mode - 16 bit displacement follows
 			case 0b10: {
 				instruction[2] = fgetc(fptr);
 				instruction[3] = fgetc(fptr);
+				uint16_t displacement =
+					two_byte_integer(instruction[3], instruction[2]);
+				if (d_field) {
+					dest = w_field ? register_word_table[reg_field]
+								   : register_byte_table[reg_field];
+					src = rm_mem_mode[rm_field];
+					char *temp = malloc(strlen(src) + 8);
+					if (displacement > 0)
+						sprintf(temp, "%s + %d", src, displacement);
+					else strcpy(temp, src);
+					src = surround_string(temp, "[%s]");
+					free(temp);
+				} else {
+					dest = rm_mem_mode[rm_field];
+					src = w_field ? register_word_table[reg_field]
+								  : register_byte_table[reg_field];
+					char *temp = malloc(strlen(dest) + 8);
+					if (displacement > 0)
+						sprintf(temp, "%s + %d", dest, displacement);
+					else strcpy(temp, dest);
+					dest = surround_string(temp, "[%s]");
+					free(temp);
+				}
+			} break;
+			// No displacement - except when R/M == 0b110, then 16 bit
+			// displacement follows
+			case 0b00: {
+				// Direct address - 16 bit - TODO
+				if (rm_field == 0b110) {
+					instruction[2] = fgetc(fptr);
+					instruction[3] = fgetc(fptr);
+					break;
+				}
+				if (d_field) {
+					dest = w_field ? register_word_table[reg_field]
+								   : register_byte_table[reg_field];
+					src = rm_mem_mode[rm_field];
+					src = surround_string(src, "[%s]");
+				} else {
+					dest = rm_mem_mode[rm_field];
+					src = w_field ? register_word_table[reg_field]
+								  : register_byte_table[reg_field];
+					dest = surround_string(dest, "[%s]");
+				}
 			} break;
 			}
 
@@ -85,14 +167,14 @@ int main(int argc, char **argv) {
 				char reg_field = instruction[0] & 0b111;
 
 				char *dest = w_field ? register_word_table[reg_field]
-							   : register_byte_table[reg_field];
+									 : register_byte_table[reg_field];
 				int16_t imval;
 				if (w_field) {
 					instruction[2] = fgetc(fptr);
 
 					unsigned char low = instruction[1];
-					int16_t high = instruction[2] << 8;
-					imval = low | high;
+					char high = instruction[2];
+					imval = two_byte_integer(high, low);
 				} else {
 					imval = instruction[1];
 				}
